57d27691b535fbb9787314ef5fb88f5d5408cba7
longyun <longyun@linklogis.com>
2022-07-29 21:35:44 +0800

    提供 mybatis-plus 分页配置

---
+++ wheel-admin/src/main/java/com/wheel/admin/config/MyBatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.wheel.admin.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description: mybatis-plus 分页配置
+ *
+ * @author longyun
+ * @version 1.0
+ * @date 2022/7/29 21:34
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    /**
+     * 分页插件
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+}
