3591f63cb4b8e40e77938627a71a240421f272b4
longyun <longyun@linklogis.com>
2022-07-29 21:27:14 +0800

    SwaggerConfiguration 配置实现

---
+++ wheel-admin/src/main/java/com/wheel/admin/config/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+package com.wheel.admin.config;
+
+import io.swagger.annotations.ApiModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Description: swagger 配置
+ *
+ * @author longyun
+ * @version 1.0
+ * @date 2022/7/29 21:24
+ */
+@Component
+@EnableOpenApi
+@ApiModel
+public class SwaggerConfiguration {
+
+    /**
+     * 对C端用户的接口文档
+     *
+     * @return
+     */
+    @Bean
+    public Docket webApiDoc() {
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("用户端接口文档")
+                .pathMapping("/")
+                //定义是否开启Swagger，false是关闭，可以通过变量去控制，线上关闭
+                .enable(true)
+                //配置文档的元信息
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.wheel.admin"))
+                //正则匹配请求路径，并分配到当前项目组
+                //.paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("cheetah平台")
+                .description("消息推送接口接口文档")
+                .contact(new Contact("3y", "http://github.com/yunlong826/wheel4j/tree/develop", "1653812264@qq.com"))
+                .version("v1.0")
+                .build();
+    }
+}
